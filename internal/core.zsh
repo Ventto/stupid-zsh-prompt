1d8d5c7e9467fbdc15ce97bd8d30fc6f3191cc28
Thomas Venriès <thomas.venries@gmail.com>
2020-04-27 14:13:18 +0200

    src: Fix project title in sources
    Signed-off-by: Thomas Venriès <thomas.venries@gmail.com>

--- internal/core.zsh
+++ internal/core.zsh
@@ -2,11 +2,11 @@
 
 source "$(dirname "$0")/settings.zsh"
 
-function _mypt_count() {
-    MYPROMPT_SIZE=$(($MYPROMPT_SIZE + ${#1}))
+function _szp_count() {
+    SZP_PROMPT_SIZE=$(($SZP_PROMPT_SIZE + ${#1}))
 }
 
-function _mypt_separator() {
+function _szp_separator() {
     local _symbol="$1"
     local _bg_color="$2"
     local _fg_color="$3"
@@ -17,46 +17,46 @@ function _mypt_separator() {
     [ "$_bg_color" != "" ] && bg_tag="%{\$bg[${_bg_color}]%}"
     [ "$_fg_color" != "" ] && fg_tag="%{\$fg[${_fg_color}]%}"
 
-    _mypt_count "$_symbol"
+    _szp_count "$_symbol"
     echo -n "${bg_tag}${fg_tag}${_symbol}%{\$reset_color%}"
 }
 
-function _mypt_block() {
+function _szp_block() {
     local _content="$1"
     local _bg_color="$2"
     local _fg_color="$3"
 
-    local separator="$(_mypt_separator "$MYPROMPT_SEPARATOR_LEFT_FMT" '' "${_bg_color}")"
+    local separator="$(_szp_separator "$SZP_SEPARATOR_LEFT_FMT" '' "${_bg_color}")"
     local begin_color_cfg="%{\$bg[${_bg_color}]%}%{\$fg[${_fg_color}]%}"
 
     export PROMPT="${PROMPT}${separator}${begin_color_cfg}${_content}"
 }
 
-function _mypt_prompt_cmd_status() {
+function _szp_prompt_cmd_status() {
     local _pipestatus="$1"
 
     local rc="${_pipestatus##*' '}"
 
     if [ "$rc" -eq 0 ]; then
-        local cmd_state_fmt="$MYPROMPT_CMD_STATUS_SUCCESS_FMT"
-        MYPROMPT_CMD_STATUS_COLORS=( green black )
+        local cmd_state_fmt="$SZP_CMD_STATUS_SUCCESS_FMT"
+        SZP_CMD_STATUS_COLORS=( green black )
     else
-        local cmd_state_fmt="$MYPROMPT_CMD_STATUS_FAILURE_FMT"
-        MYPROMPT_CMD_STATUS_COLORS=( red black )
+        local cmd_state_fmt="$SZP_CMD_STATUS_FAILURE_FMT"
+        SZP_CMD_STATUS_COLORS=( red black )
     fi
 
-    _mypt_block " ${_pipestatus} ${cmd_state_fmt}" "$MYPROMPT_CMD_STATUS_COLORS[1]" "$MYPROMPT_CMD_STATUS_COLORS[2]"
+    _szp_block " ${_pipestatus} ${cmd_state_fmt}" "$SZP_CMD_STATUS_COLORS[1]" "$SZP_CMD_STATUS_COLORS[2]"
 
     # Whitespace before command line
     export PROMPT="${PROMPT} "
 }
 
-function _mypt_prompt_date() {
-    _mypt_block " $MYPROMPT_DATE_FMT " "$MYPROMPT_DATE_COLORS[1]" "$MYPROMPT_DATE_COLORS[2]"
-    _mypt_count " $(date +%H:%M) "
+function _szp_prompt_date() {
+    _szp_block " $SZP_DATE_FMT " "$SZP_DATE_COLORS[1]" "$SZP_DATE_COLORS[2]"
+    _szp_count " $(date +%H:%M) "
 }
 
-function _mypt_prompt_cmd_timer() {
+function _szp_prompt_cmd_timer() {
     function time_ms_to_human_readable() {
         local _time="$1"
 
@@ -87,100 +87,100 @@ function _mypt_prompt_cmd_timer() {
         done
         echo -n "${_time}ms "
     }
-    if [ -n "$_mypt_cmd_timer" ]; then
-        local cmd_time="$(time_ms_to_human_readable "$_mypt_cmd_timer")"
-        cmd_time="${MYPROMPT_CMD_TIMER_RESULT_FMT} ${cmd_time}"
+    if [ -n "$_szp_cmd_timer" ]; then
+        local cmd_time="$(time_ms_to_human_readable "$_szp_cmd_timer")"
+        cmd_time="${SZP_CMD_TIMER_RESULT_FMT} ${cmd_time}"
     else
-        local cmd_time="${MYPROMPT_CMD_TIMER_WAIT_FMT} "
+        local cmd_time="${SZP_CMD_TIMER_WAIT_FMT} "
     fi
 
-    _mypt_block "$cmd_time" "$MYPROMPT_CMD_TIMER_COLORS[1]" "$MYPROMPT_CMD_TIMER_COLORS[2]"
-    _mypt_count "$cmd_time"
+    _szp_block "$cmd_time" "$SZP_CMD_TIMER_COLORS[1]" "$SZP_CMD_TIMER_COLORS[2]"
+    _szp_count "$cmd_time"
 }
 
-function _mypt_prompt_cdir() {
-    _mypt_block " $MYPROMPT_CDIR_FMT " "$MYPROMPT_CDIR_COLORS[1]" "$MYPROMPT_CDIR_COLORS[2]"
+function _szp_prompt_cdir() {
+    _szp_block " $SZP_CDIR_FMT " "$SZP_CDIR_COLORS[1]" "$SZP_CDIR_COLORS[2]"
     # More 2 whitespace to include the icon + whitespace
-    _mypt_count "   $(pwd | sed -e "s%${HOME}%~%") "
+    _szp_count "   $(pwd | sed -e "s%${HOME}%~%") "
 
 }
 
-function _mypt_prompt_vcs() {
+function _szp_prompt_vcs() {
     [ -z "${vcs_info_msg_0_}" ] && return
 
-    _mypt_block  " ${vcs_info_msg_0_}" "$MYPROMPT_VCS_COLORS[1]" "$MYPROMPT_VCS_COLORS[2]"
-    _mypt_count " ${vcs_info_msg_0_} "
+    _szp_block  " ${vcs_info_msg_0_}" "$SZP_VCS_COLORS[1]" "$SZP_VCS_COLORS[2]"
+    _szp_count " ${vcs_info_msg_0_} "
 }
 
-function _mypt_prompt_eol() {
+function _szp_prompt_eol() {
     local _last_colors=( "$1" )
 
     # Split string into array (based on whitespace character)
     _last_colors=("${(@s/ /)_last_colors}")
 
-    export PROMPT="${PROMPT}%{\$reset_color%}$(_mypt_separator "$MYPROMPT_SEPARATOR_RIGHT_FMT" '' "$_last_colors[1]")%{\$reset_color%}"
+    export PROMPT="${PROMPT}%{\$reset_color%}$(_szp_separator "$SZP_SEPARATOR_RIGHT_FMT" '' "$_last_colors[1]")%{\$reset_color%}"
 }
 
-function _mypt_prompt_newline() {
+function _szp_prompt_newline() {
     local _colors="$1"
 
-    _mypt_prompt_eol "$_colors"
+    _szp_prompt_eol "$_colors"
 
     # Complete the line before the next one
-    export PROMPT="${PROMPT}$(_mypt_complete_line "$(($COLUMNS-$MYPROMPT_SIZE-5))")"
-    export PROMPT="${PROMPT}"$'\n'"${MYPROMPT_LASTLINE_PREFIX_FMT}"
+    export PROMPT="${PROMPT}$(_szp_complete_line "$(($COLUMNS-$SZP_PROMPT_SIZE-5))")"
+    export PROMPT="${PROMPT}"$'\n'"${SZP_LASTLINE_PREFIX_FMT}"
 
     # Reset calculated prompt size
-    MYPROMPT_SIZE=0
+    SZP_PROMPT_SIZE=0
 }
 
-function _mypt_update_block_visiblity() {
-    MYPROMPT_VCS_IS_VISIBLE=$([ -n "$vcs_info_msg_0_" ] && echo 0 || echo 1)
+function _szp_update_block_visiblity() {
+    SZP_VCS_IS_VISIBLE=$([ -n "$vcs_info_msg_0_" ] && echo 0 || echo 1)
 }
 
-function _mypt_cmd_timer_stop() {
-    if [ -n "$_mypt_cmd_timer" ]; then
+function _szp_cmd_timer_stop() {
+    if [ -n "$_szp_cmd_timer" ]; then
         local now=$(($(date +%s%0N)/1000000))
-        local elapsed=$(($now - $_mypt_cmd_timer))
-        _mypt_cmd_timer="$elapsed"
+        local elapsed=$(($now - $_szp_cmd_timer))
+        _szp_cmd_timer="$elapsed"
     fi
 }
 
-function _mypt_complete_line() {
+function _szp_complete_line() {
     if [ "$1" -le 0 ]; then
         return
     fi
     for i in {1.."$1"}; do echo -n '─'; done
 }
 
-function _mypt_precmd() {
+function _szp_precmd() {
     local _pipestatus="$pipestatus"
 
     # Stop the command timer as soon as possible avoiding the shell overhead
-    _mypt_cmd_timer_stop
+    _szp_cmd_timer_stop
 
     # Populates Version Control Systems's variables
     vcs_info
 
     # Populates blocks visibility's variable
-    _mypt_update_block_visiblity
+    _szp_update_block_visiblity
 
     local last_colors=()
     local elm=""
 
-    for (( i = 1; i <= $#MYPROMPT_LEFT_PROMPT_ELEMENTS; i++ )) do
-        elm="$MYPROMPT_LEFT_PROMPT_ELEMENTS[i]"
+    for (( i = 1; i <= $#SZP_LEFT_PROMPT_ELEMENTS; i++ )) do
+        elm="$SZP_LEFT_PROMPT_ELEMENTS[i]"
 
         # First item
         if [ "$i" -eq 1 ]; then
-            export PROMPT="$MYPROMPT_FIRSTLINE_PREFIX_FMT"
-            _mypt_count "$MYPROMPT_FIRSTLINE_PREFIX_FMT"
+            export PROMPT="$SZP_FIRSTLINE_PREFIX_FMT"
+            _szp_count "$SZP_FIRSTLINE_PREFIX_FMT"
         fi
 
         local elm_visible=""
 
         if [ "$elm" != "newline" ]; then
-            elm_visible="MYPROMPT_${elm:u}_IS_VISIBLE"
+            elm_visible="SZP_${elm:u}_IS_VISIBLE"
             elm_visible="$(eval "echo \${${elm_visible}}")"
 
             if [ ! -z "$elm_visible" ] && [ "$elm_visible" = "1" ]; then
@@ -189,37 +189,37 @@ function _mypt_precmd() {
         fi
 
         case $elm in
-            newline)    _mypt_prompt_$elm "$last_colors";;
-            cmd_status) _mypt_prompt_$elm "$_pipestatus";;
-            *) _mypt_prompt_$elm;;
+            newline)    _szp_prompt_$elm "$last_colors";;
+            cmd_status) _szp_prompt_$elm "$_pipestatus";;
+            *) _szp_prompt_$elm;;
         esac
 
         ##
-        # Must be place after _mypt_prompt_* function calls because they can
+        # Must be place after _szp_prompt_* function calls because they can
         # update the colors (c.f _my_prompt_cmd_status() ).
         if [ "$elm" != "newline" ]; then
-            last_colors="MYPROMPT_${elm:u}_COLORS"
+            last_colors="SZP_${elm:u}_COLORS"
             last_colors="$(eval "echo \${${last_colors}}")"
         fi
     done
 
     if [ "$elm" != "newline" ]; then
-        _mypt_prompt_eol "${last_colors}"
+        _szp_prompt_eol "${last_colors}"
     fi
 
     # FIXME: whitespace option after block
     export PROMPT="${PROMPT} "
-    unset _mypt_cmd_timer
-    unset MYPROMPT_SIZE
+    unset _szp_cmd_timer
+    unset SZP_PROMPT_SIZE
 }
 
-function _mypt_preexec() {
-    _mypt_cmd_timer=$(($(date +%s%0N)/1000000))
+function _szp_preexec() {
+    _szp_cmd_timer=$(($(date +%s%0N)/1000000))
 }
 
-function _mypt_clean() {
-    unset _mypt_cmd_timer
+function _szp_clean() {
+    unset _szp_cmd_timer
 }
 
-precmd_functions=($precmd_functions _mypt_precmd _mypt_clean)
-preexec_functions=($precmd_functions _mypt_preexec)
+precmd_functions=($precmd_functions _szp_precmd _szp_clean)
+preexec_functions=($precmd_functions _szp_preexec)
--- internal/settings.zsh
+++ internal/settings.zsh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-typeset -g MYPROMPT_LEFT_PROMPT_ELEMENTS=(
+typeset -g SZP_LEFT_PROMPT_ELEMENTS=(
     # ===============[ Line #1 ]===============
     date            # current date
     cdir            # current directory
@@ -15,40 +15,40 @@ typeset -g MYPROMPT_LEFT_PROMPT_ELEMENTS=(
 #                  Formats
 # ============================================
 
-typeset -g MYPROMPT_FIRSTLINE_PREFIX_FMT='╭─'
-typeset -g MYPROMPT_LASTLINE_PREFIX_FMT='╰─'
-typeset -g MYPROMPT_DATE_FMT='%D{%H:%M}'
+typeset -g SZP_FIRSTLINE_PREFIX_FMT='╭─'
+typeset -g SZP_LASTLINE_PREFIX_FMT='╰─'
+typeset -g SZP_DATE_FMT='%D{%H:%M}'
 
 ##
 # Description:
 #
-#   Ex: typeset -g MYPROMPT_CDIR_FMT='  %(4~|%-1~/…/%3~|%~)'
+#   Ex: typeset -g SZP_CDIR_FMT='  %(4~|%-1~/…/%3~|%~)'
 #
 # This checks, if the path is at least 4 elements long (%(4~|true|false)) and,
 # if true, prints the first element (%-1~), some dots (/…/) and
 # the last 3 elements otherwise the full path is printed %~.
 #
-typeset -g MYPROMPT_CDIR_FMT='  %(4~|%-1~/…/%3~|%~)'
-typeset -g MYPROMPT_CMD_STATUS_SUCCESS_FMT=''
-typeset -g MYPROMPT_CMD_STATUS_FAILURE_FMT=''
-typeset -g MYPROMPT_CMD_TIMER_WAIT_FMT=' '
-typeset -g MYPROMPT_CMD_TIMER_RESULT_FMT=' '
+typeset -g SZP_CDIR_FMT='  %(4~|%-1~/…/%3~|%~)'
+typeset -g SZP_CMD_STATUS_SUCCESS_FMT=''
+typeset -g SZP_CMD_STATUS_FAILURE_FMT=''
+typeset -g SZP_CMD_TIMER_WAIT_FMT=' '
+typeset -g SZP_CMD_TIMER_RESULT_FMT=' '
 
-typeset -g MYPROMPT_SEPARATOR_LEFT_FMT=''
-typeset -g MYPROMPT_SEPARATOR_RIGHT_FMT=''
+typeset -g SZP_SEPARATOR_LEFT_FMT=''
+typeset -g SZP_SEPARATOR_RIGHT_FMT=''
 
 # ============================================
 #                   Colors
 # ============================================
 
-typeset -g MYPROMPT_CMD_TIMER_COLORS=( yellow black )
-typeset -g MYPROMPT_DATE_COLORS=( blue black )
-typeset -g MYPROMPT_CDIR_COLORS=( white black)
-typeset -g MYPROMPT_VCS_COLORS=( yellow black )
-typeset -g MYPROMPT_CMD_STATUS_COLORS=( green black )
+typeset -g SZP_CMD_TIMER_COLORS=( yellow black )
+typeset -g SZP_DATE_COLORS=( blue black )
+typeset -g SZP_CDIR_COLORS=( white black)
+typeset -g SZP_VCS_COLORS=( yellow black )
+typeset -g SZP_CMD_STATUS_COLORS=( green black )
 
 # ============================================
 #                 Visibility
 # ============================================
 
-typeset -g MYPROMPT_VCS_I_IS_VISIBLE=1
+typeset -g SZP_VCS_I_IS_VISIBLE=1
